26ac0d39b3507c0bd9943290df898397961306fd
corneliusjunior22-debug <corneliusjunior22@gmail.com>
2026-05-05 22:05:02 -0500

    Update feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,6 +1,9 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-vectorizer = TfidfVectorizer(max_features=5000)
+vectorizer = TfidfVectorizer(max_features=3000)
+
+def fit_transform(texts):
+    return vectorizer.fit_transform(texts)
 
 def transform(texts):
-    return vectorizer.fit_transform(texts)
+    return vectorizer.transform(texts)
