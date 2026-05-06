5a38edc7de8aba973f8d064ad40871e26edda421
corneliusjunior22-debug <corneliusjunior22@gmail.com>
2026-05-05 22:05:56 -0500

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,4 +2,4 @@ from sklearn.metrics import classification_report
 
 def evaluate(model, X_test, y_test):
     preds = model.predict(X_test)
-    print(classification_report(y_test, preds))
+    return classification_report(y_test, preds)
