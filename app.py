0ea6af31a9f91d5ef8c80af9d70a6b0cfdd317be
corneliusjunior22-debug <corneliusjunior22@gmail.com>
2026-05-05 22:22:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import sys
 import os
+st.write(os.listdir())
+st.write(os.listdir("src"))
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
